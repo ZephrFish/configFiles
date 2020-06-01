da9f3d2b852639822d1e3eeba53095d81a16f523
ZephrFish <ZephrFish@users.noreply.github.com>
2020-06-01 01:40:16 +0100

    Update zsh-setup.sh

--- zsh-setup.sh
+++ zsh-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Zsh Setup
-# Assumes a debian or ubuntu base
+# Assumes a debian or ubuntu base OR WSL
 
 
 # Download oh-my-zsh
@@ -15,8 +15,17 @@ echo "Now go add zsh-autosuggestions under ~/.zshrc plugins section"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 echo "Now go add zsh-syntax-highlighting under ~/.zshrc plugins section"
 
+# Add additional theme
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+echo 'Now go change theme to ZSH_THEME="powerlevel10k/powerlevel10k" under ~/.zshrc plugins section'
+
+# Download Font
+cd /tmp/
+wget https://github.com/microsoft/cascadia-code/releases/download/v2005.15/CascadiaCode_2005.15.zip
+unzip CascadiaCode_2005.15.zip
+
 # Add the plugins to zsh config
-# sed -i '/plugins=\{git/zsh-autosuggestions' ~/.zshrc
+sed -i '/plugins=\{git/zsh-autosuggestions' ~/.zshrc
 
 # [Optional] Uncomment if you want powerline
 # sudo apt-get install fonts-powerline
